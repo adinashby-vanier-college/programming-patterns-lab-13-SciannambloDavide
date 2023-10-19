1483db43a50b8bf5ac8abaac47ed5ac73eae3270
Davide Sciannamblo <2276744@edu.vaniercollege.qc.ca>
2023-10-19 12:49:29 -0400

    Solution

--- src/main/java/com/prog2/labs/PersonFactory.java
+++ src/main/java/com/prog2/labs/PersonFactory.java
@@ -2,8 +2,12 @@ package com.prog2.labs;
 
 public class PersonFactory {
 
-	public Person createPerson(String name) {
-		// todo: implement
-    return null;
-	}
+    private int idCounter;
+
+    public Person createPerson(String name) {
+        // todo: implement
+        Person p1 = new Person(idCounter, name);
+        idCounter++;
+        return p1;
+    }
 }
--- src/main/java/com/prog2/labs/SquareToRectangleAdapter.java
+++ src/main/java/com/prog2/labs/SquareToRectangleAdapter.java
@@ -2,7 +2,19 @@ package com.prog2.labs;
 
 public class SquareToRectangleAdapter implements Rectangle {
 
+    private Square square;
+
     public SquareToRectangleAdapter(Square square) {
-      // todo: implement
+        this.square = square;
+    }
+
+    @Override
+    public int getWidth() {
+        return square.side;
+    }
+
+    @Override
+    public int getHeight() {
+        return square.side;
     }
 }
